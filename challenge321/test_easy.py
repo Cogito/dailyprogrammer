54dfc18de30ad6894472972a697d67c1aa733469
Andrew Ardill <andrew.ardill@gmail.com>
2018-01-02 13:10:34 +1100

    challenge 321 easy: Parametrise tests

--- challenge321/test_easy.py
+++ challenge321/test_easy.py
@@ -1,14 +1,16 @@
+import pytest
 from easy import *
 
+test_data = [
+    ("00:00", "It's twelve am"),
+    ("01:30", "It's one thirty am"),
+    ("12:05", "It's twelve oh five pm"),
+    ("14:01", "It's two oh one pm"),
+    ("20:29", "It's eight twenty nine pm"),
+    ("21:00", "It's nine pm"),
+]
 
-def test_time_talking():
-    test_cases = (
-        ("00:00", "It's twelve am"),
-        ("01:30", "It's one thirty am"),
-        ("12:05", "It's twelve oh five pm"),
-        ("14:01", "It's two oh one pm"),
-        ("20:29", "It's eight twenty nine pm"),
-        ("21:00", "It's nine pm"),
-    )
-    for (case, expected) in test_cases:
-        assert time_to_text(case) == expected
+
+@pytest.mark.parametrize("a, expected", test_data)
+def test_time_talking(a, expected):
+    assert time_to_text(a) == expected
