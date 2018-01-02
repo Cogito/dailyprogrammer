e9ea6cebc8e79bb19c0b117f6f664243415e5100
Andrew Ardill <andrew.ardill@gmail.com>
2018-01-02 13:14:08 +1100

    challenge 321 easy: implement solution

--- challenge321/easy.py
+++ challenge321/easy.py
@@ -1,4 +1,10 @@
-def time_to_text(input_string):
-    time = input_string
-    output = f"It's {time}"
+def time_to_text(time):
+    hour, minute = map(int, time.split(":"))
+    meridian = "am" if hour < 12 else "pm"
+    teens = ["twelve ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ",
+             "ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ", "sixteen ", "seventeen ", "eighteen ", "nineteen "]
+    hour = teens[hour % 12]
+    minute_text = ["twenty ", "thirty ", "fourty ", "fifty ", "sixty "][minute // 10 - 2] if minute > 19 else "oh " if 0 < minute < 10 else ""
+    minute_text += teens[minute % 20 if 0 < minute < 20 else minute % 10] if minute % 10 > 0 else ""
+    output = f"It's {hour}{minute_text}{meridian}"
     return output
